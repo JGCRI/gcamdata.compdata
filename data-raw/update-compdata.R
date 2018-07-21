a2113c00474ef16487494e123653686eb2fda1d6
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2018-07-21 14:00:11 -0400

    Update for #1035

--- data-raw/update-compdata.R
+++ data-raw/update-compdata.R
@@ -5,9 +5,10 @@ outputs_dir <- "./outputs"
 comment_char <- "#"
 COMPDATA <- list()
 for(output_fn in list.files(outputs_dir, pattern="*.csv", full.names = TRUE)) {
-    dataname <- sub('.csv$', '', basename(output_fn))
-    data <- read_csv(output_fn, comment = comment_char)
-    COMPDATA[[dataname]] <- data
+  cat("Reading", output_fn, "\n")
+  dataname <- sub('.csv$', '', basename(output_fn))
+  data <- read_csv(output_fn, comment = comment_char)
+  COMPDATA[[dataname]] <- data
 }
 
 use_data(COMPDATA, overwrite = TRUE, internal = FALSE, compress = "xz")
