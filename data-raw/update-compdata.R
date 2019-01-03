c057a716393b175c0153370d2f05f4a8315da379
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2019-01-02 21:51:37 -0500

    Code, readr requirement, documentation updates

--- data-raw/update-compdata.R
+++ data-raw/update-compdata.R
@@ -1,10 +1,16 @@
 library(devtools)
 library(readr)
+library(gcamdata.compdata)
+
+# Note that if we *delete* a data output from gcamdata, it may 'hang around'
+# in COMPDATA and won't be deleted automatically. We have a good reason for
+# doing it this way, though.
+
+data("COMPDATA")   # load as an object that we'll modify and write back out below
 
 outputs_dir <- "./outputs"
 comment_char <- "#"
-COMPDATA <- list()
-for(output_fn in list.files(outputs_dir, pattern="*.csv", full.names = TRUE)) {
+for(output_fn in list.files(outputs_dir, pattern = "*.csv", full.names = TRUE)) {
     dataname <- sub('.csv$', '', basename(output_fn))
     data <- read_csv(output_fn, comment = comment_char)
     COMPDATA[[dataname]] <- data
